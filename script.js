fb3e4516b8615f80fc972834a2267c4f438e0563
unknown <vamshireddy1346@gmail.com>
2021-11-30 11:41:09 +0530

    whatsapp

---
+++ script.js
@@ -0,0 +1,14 @@
+//assigning variables
+
+var num,msg;
+function sendMsg(){
+    num=document.getElementById("numb").value;
+    msg=document.getElementById("message").value;
+
+    try{
+        window.open("https://wa.me/"+num+"?text="+msg,"_blank").focus()
+    }catch(e){
+        //for unstopported browser
+        window.location.assign("https://wa.me/"+num+"?text="+msg,"_blank");
+    }
+}
